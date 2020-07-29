eb0c6bd80f5fcfccb01646b8dc52e1705132c385
MananDesai54 <manan5401desai@gmail.com>
2020-07-29 15:46:16 +0530

    home page

--- js/home.js
+++ js/home.js
@@ -1,6 +1,14 @@
 const signUpWithEmail = document.getElementById('email');
 const cards = document.querySelector('.cards');
+const signUpForm = document.querySelector('.signUp');
+const backBtn = document.querySelector('.back-btn');
 
-signUpWithEmail.addEventListener('click',(e)=>{
+signUpWithEmail.addEventListener('click',()=>{
     cards.classList.add('go-left');
+    signUpForm.classList.remove('go-right');
+});
+
+backBtn.addEventListener('click',()=>{
+    cards.classList.remove('go-left');
+    signUpForm.classList.add('go-right');
 })
