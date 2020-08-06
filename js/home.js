2d0c7cc08d8c98ad39508d4be90b91773505f2b5
MananDesai54 <manan5401desai@gmail.com>
2020-08-06 11:29:06 +0530

    intersection observer

--- js/home.js
+++ js/home.js
@@ -14,3 +14,33 @@ backBtn.addEventListener('click',()=>{
     cards.classList.remove('go-left');
     signUpForm.classList.add('go-right');
 })
+
+
+//Intersection observer
+const HowToAnimation = document.querySelector('#how-to-use .feature__animation');
+const cardData = document.querySelectorAll('.card__data');
+
+const intersection = new IntersectionObserver((entries,observer)=>{
+    entries.forEach(entry=>{
+        if(entry.target.id === 'feature-animation') {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('animation');
+            }else {
+                entry.target.classList.remove('animation');
+            }
+        }else if(entry.target.id.includes('card__data')) {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('show');
+                intersection.unobserve(entry.target);
+            }
+        }
+    })
+},{
+    threshold:0,
+    rootMargin:'-100px'
+});
+
+intersection.observe(HowToAnimation);
+cardData.forEach(card=>{
+    intersection.observe(card);
+})
