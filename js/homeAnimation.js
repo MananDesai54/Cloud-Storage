a799e8b82872e4c9332454f210ccd00071924266
MananDesai54 <manan5401desai@gmail.com>
2020-08-06 19:12:29 +0530

    animation

---
+++ js/homeAnimation.js
@@ -0,0 +1,31 @@
+const timeline = gsap.timeline({
+    defaults:{
+        duration:0.5
+    }
+});
+
+timeline
+    .from('.intro',{
+        x:'-100%',
+        borderRadius:'100%',
+        opacity:0,
+    })
+    .from('.intro__data',{
+        opacity:0,
+        stagger:0.2
+    },'+=0.2')
+    .from('.forms',{
+        opacity:0
+    })
+    .from('.what-can-you-do',{
+        opacity:0
+    })
+    .from('.cards__info',{
+        opacity:0
+    })
+    .from('.how-to-use',{
+        opacity:0
+    })
+    .from('footer',{
+        opacity:0
+    })
