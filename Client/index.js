ecc2b9f017a59450c7d19b782fdea1fe4eb369c7
MananDesai54 <manan5401desai@gmail.com>
2020-09-13 22:14:18 +0530

    awesome man

--- Client/index.js
+++ Client/index.js
@@ -4,4 +4,10 @@ const menu = $('.menu');
 menuToggle.click(() => {
     menuToggle.toggleClass('toggle-move');
     menu.toggleClass('menu-move');
+});
+
+const roller = $('.roller');
+const switchBtn = $('.switch')
+roller.click(() => {
+    switchBtn.toggleClass('toggle-switch');
 })
